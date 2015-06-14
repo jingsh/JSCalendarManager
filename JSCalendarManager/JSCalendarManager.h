2dea6e8df712daeb13fb9bf7586969072766da88
Jing <shanjing.titan@gmail.com>
2015-06-14 17:06:48 -0400

    Initial commit.

---
+++ JSCalendarManager/JSCalendarManager.h
@@ -0,0 +1,15 @@
+//
+//  JSCalendarManager.h
+//  CalendarManager
+//
+//  Created by Jing Shan on 6/14/15.
+//  Copyright (c) 2015 Jing Shan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <EventKit/EventKit.h>
+
+
+@interface JSCalendarManager : NSObject
+
+@end
---
+++ JSCalendarManager/JSCalendarManager.m
@@ -0,0 +1,13 @@
+//
+//  JSCalendarManager.m
+//  CalendarManager
+//
+//  Created by Jing Shan on 6/14/15.
+//  Copyright (c) 2015 Jing Shan. All rights reserved.
+//
+
+#import "JSCalendarManager.h"
+
+@implementation JSCalendarManager
+
+@end
