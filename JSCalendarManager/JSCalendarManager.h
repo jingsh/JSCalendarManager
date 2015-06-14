cb36bb006aa23087b27e93ee84ee4cf70372d15a
Jing <shanjing.titan@gmail.com>
2015-06-14 17:59:28 -0400

    - Access determination. - Get available calendars.

--- JSCalendarManager/JSCalendarManager.h
+++ JSCalendarManager/JSCalendarManager.h
@@ -9,7 +9,89 @@
 #import <Foundation/Foundation.h>
 #import <EventKit/EventKit.h>
 
+static NSString *JSCalendarManagerErrorDomain = @"JSCalendarManagerErrorDomain";
+
+typedef enum:NSInteger{
+	kErrorCalendarAccessNotGranted = 1001,
+}JSCalendarManagerErrorCode;
+
+typedef void (^calendarOperationCompletionHandler)(BOOL success, NSError *error, NSString *calendarIdentifier);
+typedef void (^eventsOperationCompletionHandler)(BOOL success, NSError *error, NSString *eventIdentifier);
 
 @interface JSCalendarManager : NSObject
+
++(instancetype)sharedManager;
+
++(BOOL)calendarAccessGranted;
+
++(BOOL)shouldRequestCalendarAccess;
+
++(BOOL)shouldGrantAccessManually;
+
+-(void)requestCalendarAccessWithCompletionHandler:(EKEventStoreRequestAccessCompletionHandler)handler;
+
+-(BOOL)isICloudCalendarAvailable;
+
+-(NSArray *)nonBirthDayCalendars;
+
+-(NSArray *)iCloudCalendars;
+
+-(void)createCalendar:(NSString *)calendarTitle
+			   iCloud:(BOOL)icloud
+	completionHandler:(calendarOperationCompletionHandler)handler;
+
+-(void)deleteCalendar:(NSString *)calendarIdentifier
+	completionHandler:(calendarOperationCompletionHandler)handler;
+
+-(void)createEvent:(NSString *)eventTitle
+		  location:(NSString *)location
+		 startTime:(NSDate *)startDate
+		   endTime:(NSDate *)endDate
+	   description:(NSString *)description
+			   URL:(NSString *)urlString
+ completionHanlder:(eventsOperationCompletionHandler)handler;
+
+-(void)createEvent:(EKEvent *)event
+ completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEvent:(NSString *)eventIdentifier
+		 withTitle:(NSString *)title
+		  location:(NSString *)location
+		 startTime:(NSDate *)start
+		   endTime:(NSDate *)end
+	   description:(NSString *)descrition
+			   URL:(NSString *)urlString
+ completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEvent:(NSString *)eventIdentifier
+		 withEvent:(EKEvent *)event
+ completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEventTitle:(NSString *)title
+			   forEvent:(NSString *)eventIdentifier
+	  completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEventLocation:(NSString *)location
+			   forEvent:(NSString *)eventIdentifier
+	  completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEventDescription:(NSString *)description
+			   forEvent:(NSString *)eventIdentifier
+	  completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEventStartTime:(NSDate *)startTime
+			   forEvent:(NSString *)eventIdentifier
+	  completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEventEndTime:(NSDate *)endTime
+			   forEvent:(NSString *)eventIdentifier
+	  completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)updateEventURL:(NSString *)urlString
+			   forEvent:(NSString *)eventIdentifier
+	  completionHandler:(eventsOperationCompletionHandler)handler;
+
+-(void)deleteEvent:(NSString *)eventIdentifier completionHandler:(eventsOperationCompletionHandler)handler;
+
 
 @end
--- JSCalendarManager/JSCalendarManager.m
+++ JSCalendarManager/JSCalendarManager.m
@@ -8,6 +8,104 @@
 
 #import "JSCalendarManager.h"
 
+@interface JSCalendarManager ()
+@property(nonatomic,strong)EKEventStore *eventStore;
+@end
+
 @implementation JSCalendarManager
+
++(instancetype)sharedManager
+{
+	static JSCalendarManager *manager = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken,^{
+		manager = [[self alloc]init];
+	});
+	return manager;
+}
+
+-(instancetype)init
+{
+	self = [super init];
+	if (self) {
+		self.eventStore = [[EKEventStore alloc]init];
+	}
+	return self;
+}
+
+#pragma mark - Determine access to calendar
++(BOOL)calendarAccessGranted
+{
+	EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent];
+	return (status == EKAuthorizationStatusAuthorized)?YES:NO;
+}
+
++(BOOL)shouldRequestCalendarAccess
+{
+	EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent];
+	return (status == EKAuthorizationStatusNotDetermined)?YES:NO;
+}
+
++(BOOL)shouldGrantAccessManually
+{
+	EKAuthorizationStatus status = [EKEventStore authorizationStatusForEntityType:EKEntityTypeEvent];
+	if (status == EKAuthorizationStatusDenied || status == EKAuthorizationStatusRestricted) {
+		return YES;
+	}
+	else return NO;
+}
+
+-(void)requestCalendarAccessWithCompletionHandler:(EKEventStoreRequestAccessCompletionHandler)handler
+{
+	if ([JSCalendarManager shouldRequestCalendarAccess]) {
+		[self.eventStore requestAccessToEntityType:EKEntityTypeEvent completion:handler];
+	}
+	if ([JSCalendarManager calendarAccessGranted]){
+		handler(YES,nil);
+	}
+	if ([JSCalendarManager shouldGrantAccessManually]) {
+		NSError *error = [NSError errorWithDomain:JSCalendarManagerErrorDomain code:kErrorCalendarAccessNotGranted userInfo:nil];
+		handler(NO,error);
+	}
+}
+
+#pragma mark - Event store methods
+
+-(BOOL)isICloudCalendarAvailable
+{
+	for (EKSource *source in self.eventStore.sources) {
+		if (source.sourceType == EKSourceTypeCalDAV && [source.title isEqualToString:@"iCloud"]) {
+			return YES;
+		}
+	}
+	return NO;
+}
+
+-(NSArray *)nonBirthDayCalendars
+{
+	NSMutableArray *calendars = [NSMutableArray array];
+	if ([JSCalendarManager calendarAccessGranted]) {
+		for (EKCalendar *calendar in [self.eventStore calendarsForEntityType:EKEntityTypeEvent]) {
+			if (calendar.type != EKCalendarTypeBirthday) {
+				[calendars addObject:calendar];
+			}
+		}
+	}
+	return calendars;
+}
+
+-(NSArray *)iCloudCalendars
+{
+	NSMutableArray *calendars = [NSMutableArray array];
+	if ([JSCalendarManager calendarAccessGranted]) {
+		for (EKCalendar *calendar in [self.eventStore calendarsForEntityType:EKEntityTypeEvent]) {
+			if (calendar.type == EKCalendarTypeCalDAV && [calendar.source.title isEqualToString:@"iCloud"]) {
+				[calendars addObject:calendar];
+			}
+		}
+	}
+	return calendars;
+}
+
 
 @end
